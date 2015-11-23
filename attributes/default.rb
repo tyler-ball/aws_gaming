f3ba90e71c1d4998f6a3b5ec0fe2889a231c0d85
tyler-ball <tyleraball@gmail.com>
2015-11-23 12:20:02 -0700

    Using the correct user

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 require 'open-uri'
 default[:my_ip] = open('http://whatismyip.akamai.com').read.strip
-default[:aws_driver] = "aws:tester:us-west-2"
+default[:aws_driver] = "aws:#{ENV['USER']}:us-west-2"
